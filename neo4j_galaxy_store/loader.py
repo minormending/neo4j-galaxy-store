3dbc0536b0f7a45ce0087ba473fb34002178500e
minormending <54426030+minormending@users.noreply.github.com>
2022-06-01 07:21:07 -0400

    get models from mongo

--- neo4j_galaxy_store/loader.py
+++ neo4j_galaxy_store/loader.py
@@ -1,16 +1,27 @@
+from typing import Any, Dict, Iterable, List
+from pymongo.cursor import Cursor
 
-from typing import Any
-from .connection import MongoConnection, Neo4jConnection
-
-
-with Neo4jConnection(uri="bolt://localhost:7687", user="neo4j", passwd="test") as conn:
-    pass
+from connection import MongoConnection, Neo4jConnection
 
 
-class MongoLoader:
+class GalaxyStoreMongoDB:
     def __init__(self, uri: str) -> None:
         self.uri = uri
 
-    def categories(self) -> Any:
-        with MongoConnection(self.uri) as conn:
-            pass
+    def categories(self) -> Iterable[Dict[str, Any]]:
+        yield from self._get_all_collection("categories")
+
+    def apps(self) -> Iterable[Dict[str, Any]]:
+        yield from self._get_all_collection("apps", batch_size=1000)
+
+    def reviews(self) -> Iterable[Dict[str, Any]]:
+        yield from self._get_all_collection("reviews")
+
+    def _get_all_collection(
+        self, collection: str, batch_size: int = 5000
+    ) -> Iterable[Dict[str, Any]]:
+        with MongoConnection(self.uri, "samsung-galaxy-store") as database:
+            cursor: Cursor = database[collection].find({}, batch_size=batch_size)
+            yield from cursor
+
+
