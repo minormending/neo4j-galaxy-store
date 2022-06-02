28624fb587e6e33ba2dac2e9134f02975804ccbe
minormending <54426030+minormending@users.noreply.github.com>
2022-06-02 08:12:56 -0400

    insert models resuse

--- neo4j_galaxy_store/loader.py
+++ neo4j_galaxy_store/loader.py
@@ -54,21 +54,89 @@ class GalaxyStoreNeo4j:
                 db=self.db,
             )
 
-    def populate_categories(self, categories: List[Dict[str, Any]]) -> None:
+    def populate_categories(self, categories: List[Dict[str, Any]]) -> int:
+        return self._insert_model("Category", "_id", categories)
         with self._get_connection() as conn:
+            fields: str = self._build_update_query("c", categories)
             query: str = (
                 """
                 UNWIND $rows AS row
-                MERGE (c:Category {_id: row._id})
-                ON CREATE SET """
-                + self._build_update_query("c", categories)
-                + " ON MATCH SET " + self._build_update_query("c", categories)
-                + """
+                MERGE (c:Category {_id: row._id})"""
+                + f"""
+                ON CREATE SET {fields}
+                ON MATCH SET {fields}
+                RETURN count(*) as total
+            """
+            )
+            result: List[Dict[str, Any]] = conn.query(
+                query, parameters={"rows": categories}, db=self.db
+            )
+            return result[0]["total"]
+
+    def populate_apps(self, apps: List[Dict[str, Any]]) -> None:
+        total: int = 0
+        total_developers: int = 0
+
+        batch_size: int = 1000
+        for idx_start in range(0, len(apps), batch_size):
+            idx_end = min(idx_start + batch_size, len(apps))
+            batch: List[Dict[str, Any]] = apps[idx_start:idx_end]
+
+            developers: List[Dict[str, str]] = [app["developer"] for app in batch if "developer" in app]
+            with self._get_connection() as conn:
+                fields: str = self._build_update_query("d", batch)
+                query: str = (
+                    """
+                    UNWIND $rows AS row
+                    MERGE (d:Developer {name: row.name})"""
+                    + f"""
+                    ON CREATE SET {fields}
+                    ON MATCH SET {fields}
+                    RETURN count(*) as total
+                """
+                )
+                print(query)
+                result: List[Dict[str, Any]] = conn.query(
+                    query, parameters={"rows": developers}, db=self.db
+                )
+                total_developers += result[0]["total"]
+
+            with self._get_connection() as conn:
+                fields: str = self._build_update_query("a", batch)
+                query: str = (
+                    """
+                    UNWIND $rows AS row
+                    MERGE (a:App {_id: row._id})"""
+                    + f"""
+                    ON CREATE SET {fields}
+                    ON MATCH SET {fields}
+                    RETURN count(*) as total
+                """
+                )
+                print(query)
+                result: List[Dict[str, Any]] = conn.query(
+                    query, parameters={"rows": batch}, db=self.db
+                )
+                total += result[0]["total"]
+        return total
+
+    def _insert_model(self, model_name: str, merge_field: str, models: List[Dict[str, str]]) -> None:
+        with self._get_connection() as conn:
+            fields: str = self._build_update_query("i", models)
+            query: str = (
+                f"""
+                UNWIND $rows AS row
+                MERGE (i:{model_name} {{{merge_field}: row.{merge_field}}})
+                ON CREATE SET {fields}
+                ON MATCH SET {fields}
                 RETURN count(*) as total
             """
             )
             print(query)
-            return conn.query(query, parameters={"rows": categories}, db=self.db)
+            result: List[Dict[str, Any]] = conn.query(
+                query, parameters={"rows": models}, db=self.db
+            )
+            return result[0]["total"]
 
     def _build_update_query(self, model_name: str, models: List[Dict[str, Any]]) -> str:
         keys: Set[str] = set(key for model in models for key in model.keys())
@@ -79,4 +147,7 @@ mongo = GalaxyStoreMongoDB("mongodb://localhost:27017")
 neo = GalaxyStoreNeo4j(uri="bolt://localhost:7687", user="neo4j", passwd="test")
 neo.setup_constraints()
 total: int = neo.populate_categories(list(mongo.categories()))
-print(total)
+print("categories:", total)
+
+total: int = neo.populate_apps(list(mongo.apps()))
+print("apps:", total)
